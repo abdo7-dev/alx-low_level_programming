f35c259031a57f9aa8ca93e8d1b46184f8132bca
abdelrahman <abdelrahmanmorsy569@gmail.com>
2023-07-18 20:03:15 -0700

    fuckme

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,12 +12,12 @@ long a = 1;
 long b = 2;
 for (counter = 1; counter <= (countto / 2); counter++)
 {
-printf("%li %li ", a, b);
+printf("%li, %li, ", a, b);
 a += b;
 b += a;
 }
 if (countto % 2 == 1)
-printf("%li", a);
+printf("%li, ", a);
 printf("\n");
 return (0);
 }
