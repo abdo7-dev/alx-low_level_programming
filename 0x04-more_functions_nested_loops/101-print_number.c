6b6669f05a3e5e438b7218d27a129b50a13d7dbf
abdelrahman <abdelrahmanmorsy569@gmail.com>
2023-07-20 19:26:11 -0700

    m

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,7 +16,7 @@ else
 {
 if (n < 0)
 {
-positive = n * -1;
+positive = n * (-1);
 _putchar('-');
 }
 
@@ -28,7 +28,7 @@ while (tens >= 1)
 {
 digit = positive / tens;
 _putchar(digit + '0');
-positive = (positive - (tens * digit));
+positive = (positive - tens *digit);
 tens /= 10;
 }
 }
