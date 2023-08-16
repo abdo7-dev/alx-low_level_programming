fbe40b7447444648a1b2e8e20d9376781fc9214a
abdelrahman <abdelrahmanmorsy569@gmail.com>
2023-08-16 09:10:30 -0700

    second

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,31 +1,15 @@
-#include "function_pointers.h"
+#ifndef CALCF
+#define CALCF
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * int_index - ...
- * @array: ...
- * @size: ...
- * @cmp: ...
+ * struct op - Short description
+ * @op: the operator to ve examined
+ * @f: pointer to function.
  *
- * Return: ...
- */
-int int_index(int *array, int size, int (*cmp)(int))
-{
-int i = 0;
-
-if (size > 0)
-{
-if (array != NULL && cmp != NULL)
-{
-while (i < size)
-{
-if (cmp(array[i]))
-#ifndef _HEADER_
-#define _HEADER_
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * Description: Longer description
  */
 typedef struct op
 {
@@ -33,19 +17,11 @@ char *op;
 int (*f)(int a, int b);
 } op_t;
 
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+
 #endif
-return (i);
-
-i++;
-}
-}
-}
-
-return (-1);
-}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,64 +1,37 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * get_op_func - ...
- * @s: ...
- *
- * Return: ...
- */
-int (*get_op_func(char *s))(int, int)
+ * main - Entry point
+ * @argc: the number of the parameters
+ * @argv: the parameters in the case the number to be calculated.
+(* a blank line
+* Description: this program is the enttry point for a calculator)?
+(* section header: 3-calc.h)*
+* Return: 0 in success
+*/
+int main(int argc, char *argv[])
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}};
-int i = 0;
-#include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+int n1, n2, result;
+int (*p)(int, int);
 
-/**
- * main - ...
- * @argc: ...
- * @argv: ...
- *
- * Return: ...
- */
-int
-main(int argc, char *argv[])
-{
-int (*oprt)(int, int);
-
-if (argc != 4)
+if (argc < 4 || argc > 4)
 {
 printf("Error\n");
 exit(98);
 }
 
-oprt = get_op_func(argv[2]);
+n1 = atoi(argv[1]);
+n2 = atoi(argv[3]);
 
-if (!oprt)
+p = get_op_func(argv[2]);
+
+if (p == NULL)
 {
 printf("Error\n");
 exit(99);
 }
+result = p(n1, n2);
 
-printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-return (0);
-}
-
-while (i < 5)
-{
-if (strcmp(s, ops[i].op) == 0)
-return (ops[i].f);
-
-i++;
-}
-
+printf("%d\n", result);
 return (0);
 }
